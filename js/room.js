b62619fbc0243a3d9bfa40213dc7bbf53ba2644f
Andrew Tibbetts <andrewgtibbetts@gmail.com>
2026-05-17 21:49:41 -0500

    #114 hide bus button for jarvis

--- js/room.js
+++ js/room.js
@@ -680,7 +680,8 @@ firetable.ui.setupRoomEvents = function () {
           var isSelf = userId === ftapi.uid;
           var ownUser = ftapi.uid && ftapi.users && ftapi.users[ftapi.uid];
           var isMod = ownUser && (ownUser.mod || ownUser.supermod);
-          var showDeparture = isSelf || isMod;
+          var isHostbot = !!(userInfo && userInfo.hostbot);
+          var showDeparture = (isSelf || isMod) && !isHostbot;
           var removeAfterValue = data[key].removeAfter;
           var wlName = firetable.utilities.htmlEscape(data[key].name);
           var departureTitleOff = isSelf ? 'Step down after your next play' : 'Have ' + wlName + ' step down after their next play';
@@ -736,14 +737,16 @@ firetable.ui.setupRoomEvents = function () {
           var isSelf = data[key].id === ftapi.uid;
           var ownUser = ftapi.uid && ftapi.users && ftapi.users[ftapi.uid];
           var isMod = ownUser && (ownUser.mod || ownUser.supermod);
+          var isHostbot = !!(ftapi.users && ftapi.users[data[key].id] && ftapi.users[data[key].id].hostbot);
           var showBtn = isSelf || isMod;
+          var showDeparture = showBtn && !isHostbot;
           var btnIcon = isSelf ? 'close' : 'person_remove';
           var btnTitle = isSelf ? 'Step down' : 'Remove from deck';
           var actionBtn = showBtn
             ? '<button class="iconbutt deckRemoveBtn" data-userid="' + data[key].id + '" data-tablekey="' + key + '" title="' + btnTitle + '"><i class="material-symbols-filled">' + btnIcon + '</i></button>'
             : '';
           var departureIndicator;
-          if (showBtn) {
+          if (showDeparture) {
             var isSelfDj = data[key].id === ftapi.uid;
             var djDisplayName = firetable.utilities.htmlEscape(data[key].name);
             // Use pending state if a bot command is in-flight, else use Firebase value
